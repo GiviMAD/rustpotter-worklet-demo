edf8bc3a697152059cd9c9bc8a6f5bd37810eca4
Miguel Álvarez <miguelwork92@gmail.com>
2022-06-02 00:24:14 +0200

    working demo page

--- js/index.js
+++ js/index.js
@@ -1,44 +1,194 @@
+/** @typedef { import('rustpotter-worklet').RustpotterService } RustpotterService */
 (async () => {
-    const wasmModuleUrl = new URL('../node_modules/rustpotter-worklet/dist/rustpotter_wasm_bg.wasm', import.meta.url);
-    const workletModuleUrl = new URL('../node_modules/rustpotter-worklet/dist/rustpotterWorklet.js', import.meta.url);
+    const USER_NAME = "rustpotter@Demo";
+    let state = {
+        selectedWakeword: null,
+        selectedWakewordBytes: null,
+        selectedThreshold: 0.5,
+        selectedAvgThreshold: 0.,
+        eagerMode: true,
+        /**
+         * @type {RustpotterService}
+         */
+        rustpotterService: null,
+    };
+    window.addEventListener('load', onWindowsLoad());
     try {
-        const { RustpotterService } = await import("rustpotter-worklet");
-        if (!RustpotterService.isRecordingSupported()) {
-            alert("Unable to record in this browser :(");
-        }
-        var el = document.querySelector("#record");
-        el.addEventListener('click', async () => {
-            var rustpotterService = new RustpotterService({
+        await checkRecordCapabilities();
+        document.querySelector("#wakeword_selector").addEventListener('change', onWakewordSelectionChange);
+        document.querySelector("#wakeword_threshold").addEventListener('input', onThresholdInput);
+        document.querySelector("#wakeword_avg_threshold").addEventListener('input', onAvgThresholdInput);
+        document.querySelector("#eager_mode").addEventListener('input', onEagerModeChecked);
+        document.querySelector("#record").addEventListener('click', onRecordClick);
+        document.querySelector("#pause").addEventListener('click', onPauseClick);
+        document.querySelector("#stop").addEventListener('click', onStopClick());
+    } catch (error) {
+        printError(error.message ?? error);
+    }
+    // event listeners
+    function onThresholdInput(ev) {
+        state.selectedThreshold = ev.target.value;
+    }
+    function onAvgThresholdInput(ev) {
+        state.selectedAvgThreshold = ev.target.value;
+    }
+    function onEagerModeChecked(ev) {
+        state.eagerMode = ev.target.checked;
+    }
+    function onStopClick() {
+        return async () => {
+            enableButtons(false);
+            state.rustpotterService.stop().then(state.rustpotterService.close()).then(() => {
+                state.rustpotterService = null;
+                enableElement('record');
+                enableOptions(true);
+            }).catch(err => {
+                printError(err.message ?? err);
+            });
+        };
+    }
+    function onPauseClick() {
+        enableButtons(false);
+        state.rustpotterService.pause().then(() => {
+            enableElement('record');
+        }).catch(err => {
+            printError(err.message ?? err);
+        });
+    }
+    async function onRecordClick() {
+        try {
+            const { RustpotterService } = await import("rustpotter-worklet");
+            enableElement("record", false);
+            enableOptions(false);
+            if (state.rustpotterService != null) {
+                state.rustpotterService.resume();
+                enableElement("pause");
+                enableElement("stop");
+                return;
+            }
+            const wasmModuleUrl = new URL('../node_modules/rustpotter-worklet/dist/rustpotter_wasm_bg.wasm', import.meta.url);
+            const workletModuleUrl = new URL('../node_modules/rustpotter-worklet/dist/rustpotterWorklet.js', import.meta.url);
+            state.rustpotterService = new RustpotterService({
                 workletPath: workletModuleUrl.href,
                 wasmPath: wasmModuleUrl.href,
-                averagedThreshold: 0.25,
-                threshold: 0.662,
+                averagedThreshold: state.selectedAvgThreshold,
+                threshold: state.selectedThreshold,
+                eagerMode: state.eagerMode,
             });
-            rustpotterService.onspot = (name, score) => {
-                console.log(`detection: '${name}'[${score}]`);
+            state.rustpotterService.onspot = (name, score) => {
+                printLog(`detection: '${name}'[${score}]`);
             };
-            rustpotterService.onstart = function () {
-                console.info('rustpotterService is started');
-            };
-
-            rustpotterService.onstop = function () {
-                console.info('rustpotterService is stopped');
+            state.rustpotterService.onstart = function () {
+                printLog('rustpotterService is started');
             };
 
-            rustpotterService.onpause = function () {
-                console.info('rustpotterService is paused');
+            state.rustpotterService.onstop = function () {
+                printLog('rustpotterService is stopped');
             };
 
-            rustpotterService.onresume = function () {
-                console.info('rustpotterService is resuming');
+            state.rustpotterService.onpause = function () {
+                printLog('rustpotterService is paused');
             };
-            rustpotterService
-                .start()
-                .then(() => rustpotterService.addWakewordByPath("wakeword.rpw"))
-                .catch(err => console.error(err));
-        });
-    } catch (error) {
-        console.error(error);
+            state.rustpotterService.onresume = function () {
+                printLog('rustpotterService is resumed');
+            };
+            await state.rustpotterService.start();
+            if (state.selectedWakeword == 'manual') {
+                state.rustpotterService.addWakeword(state.selectedWakewordBytes);
+            } else {
+                state.rustpotterService.addWakewordByPath(state.selectedWakeword);
+            }
+            enableElement("pause");
+            enableElement("stop");
+        } catch (error) {
+            printError(error.message ?? err);
+        }
+    }
+    function onWakewordSelectionChange(ev) {
+        const selected_value = ev.target.value;
+        state.selectedWakewordBytes = null;
+        if (selected_value == "manual") {
+            enableButtons(false);
+            const wakeword_panel = document.querySelector("#wakeword_input_container");
+            wakeword_panel.innerHTML += '<input id="wakeword_input" type="file" accept=".rpw">';
+            document.querySelector("#wakeword_input").addEventListener('change', onFileInputChange);
+        } else {
+            let input = document.querySelector("#wakeword_input");
+            if (input) input.remove();
+            enableElement('record');
+        }
+        state.selectedWakeword = selected_value;
+    }
+    function onFileInputChange(evt) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            state.selectedWakewordBytes = this.result;
+            enableElement('record');
+        };
+        reader.onerror = function () {
+            printError("Unable to read wakeword file.");
+            enableButtons(false);
+        };
+        reader.readAsArrayBuffer(this.files[0]);
+    }
+
+    function onWindowsLoad() {
+        printLog("loading available wakewords...");
+        enableButtons(false);
+        enableOptions(false);
+        fetch("wakewords.json")
+            .then((resp) => resp.json())
+            .then((wakewords) => {
+                const wakeword_selector = document.querySelector("#wakeword_selector");
+                const wakewordEntries = Object.entries(wakewords);
+                wakewordEntries.forEach((entry) => {
+                    wakeword_selector.innerHTML += '<option value="' + entry[1] + '">' + entry[0] + '</option>';
+                });
+                wakeword_selector.innerHTML += '<option value="manual">from file...</option>';
+                state.selectedWakeword = wakewordEntries[0][1];
+                wakeword_selector.disabled = false;
+                enableOptions(true);
+                enableElement('record');
+                printLog("wakewords list loaded");
+            })
+            .catch(err => {
+                printError(err.message ?? err);
+                enableButtons(false);
+            });
+    }
+
+    // utils
+    async function checkRecordCapabilities() {
+        const { RustpotterService } = await import("rustpotter-worklet");
+        if (!RustpotterService.isRecordingSupported()) {
+            const errorMessage = "Unable to record in this browser :(";
+            alert(errorMessage);
+            throw new Error(errorMessage);
+        }
+    }
+    function enableElement(id, enabled = true) {
+        document.querySelector("#" + id).disabled = !enabled;
+    }
+    function enableOptions(enabled) {
+        document.querySelector("#wakeword_panel")
+            .querySelectorAll("input,select")
+            .forEach(btn => btn.disabled = !enabled);
+    }
+    function enableButtons(enabled) {
+        document.querySelector("#btn_panel")
+            .querySelectorAll("button")
+            .forEach(btn => btn.disabled = !enabled);
+    }
+    function printLog(str) {
+        console.log(str);
+        var el = document.querySelector("#terminal");
+        el.innerHTML += '<span style="color: grey">' + USER_NAME + '</span> % <span class="ok">' + str + '</span><br>';
+        el.parentElement.scrollTop = el.parentElement.scrollHeight;
+    }
+    function printError(str) {
+        console.error(str);
+        var el = document.querySelector("#terminal");
+        el.innerHTML += '<span style="color: grey">' + USER_NAME + '</span> % <span class="error">' + str + '</span><br>';
+        el.parentElement.scrollTop = el.parentElement.scrollHeight;
     }
 })();
-
---
+++ tools/google_tts.sh
@@ -0,0 +1,12 @@
+export GOOGLE_APPLICATION_CREDENTIALS=./credentials.json
+TEXT="hey home"
+LANGUAGE="es-us"
+VOICES=('en-US-Standard-A' 'en-US-Standard-B' 'en-US-Standard-C' 'en-US-Standard-D' 'en-US-Standard-E' 'en-US-Standard-F' 'en-US-Standard-G' 'en-US-Standard-H' 'en-US-Standard-I' 'en-US-Standard-J')
+for VOICE in "${VOICES[@]}" ;do
+    JSON='{ "input":{ "text":"'$TEXT'." }, "voice":{ "languageCode":"'$LANGUAGE'", "name":"'$VOICE'" }, "audioConfig":{ "audioEncoding":"LINEAR16", "sampleRateHertz": 16000 } }';
+    curl -X POST \
+    -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+    -H "Content-Type: application/json; charset=utf-8" \
+    -d "$JSON" \
+    "https://texttospeech.googleapis.com/v1/text:synthesize" | jq -r '.audioContent' | base64 -d > ${TEXT}_g_${VOICE}.wav
+done
