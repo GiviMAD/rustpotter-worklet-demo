70084a4174d7752144b961bc34fa0afa9536eee2
Miguel Álvarez <miguelwork92@gmail.com>
2022-06-02 17:11:52 +0200

    add build:web npm script

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const dist = path.resolve(__dirname, "dist");
-
+const env = process.env.NODE_ENV === "production" ? "production" : "development"
+const dist = env === "production" ? path.resolve(__dirname, "docs") : path.resolve(__dirname, "dist");
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  mode: "development",
+  mode: env,
   performance: {
-    maxAssetSize: 300000,
+    maxAssetSize: 290000,
   },
   entry: {
     index: "./js/index.js"
@@ -15,7 +15,7 @@ module.exports = {
     path: dist,
   },
   devServer: {
-    static :"/"
+    static: "/"
   },
   plugins: [
     new CopyPlugin({
